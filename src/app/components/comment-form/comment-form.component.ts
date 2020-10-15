bb0404a8d000a8fb536eba0d47061969df1bc2ae
gabepetersen <precariousplant@gmail.com>
2020-10-14 20:18:42 -0700

    chore: added alert service

--- src/app/components/comment-form/comment-form.component.ts
+++ src/app/components/comment-form/comment-form.component.ts
@@ -10,7 +10,7 @@ import { DevRantService } from 'src/services/devrant.service';
 export class CommentFormComponent implements OnInit {
 
   @Input() rantId: number;
-  private commentString: string = '';
+  
 
   constructor(
     private service: DevRantService,
@@ -25,29 +25,6 @@ export class CommentFormComponent implements OnInit {
     await this.modalController.dismiss();
   }
 
-  getText($event) {
-    this.commentString = $event.target.value;
-  }
-
-  /**
-   * Submit the comment with the API
-   */
-  submit() {
-    // length checks on text area
-    if (this.commentString.length < 1) {
-      console.log('Comment length has to be greater than 1');
-      this.showToast('Empty Comment - Type more things!!');
-      return;
-    } else if (this.commentString.length >= 1000) {
-      console.log('Comment characters cannot exceed 1000');
-      this.showToast('Comment length cannot exceed 1000 characters');
-      return;
-    }
-
-    const token = this.service.token;
-    const response = this.service.postComment(this.rantId, this.commentString, token);
-    console.log("comment response: ", response);
-  }
 
   async showToast(msg: string) {
     const toast = await this.toastController.create({
