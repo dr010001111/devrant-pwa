493b085b3e705e5eeae9b9a4d759c01c736e9a1f
dr010001111 <dr010001111@users.noreply.github.com>
2020-10-15 22:26:15 +0200

    fix: Bad merge request

--- src/services/devrant.service.ts
+++ src/services/devrant.service.ts
@@ -24,11 +24,36 @@ export class DevRantService {
     worker: ServiceWorker;
 
     private _token: devRant.Token = null;
-
     private _profile?: devRant.Profile;
 
+    userIdCache: Cache;
+    activeUserIdQueue = {};
+
     async getUserIdByName(username) {
-        return devRant.getIdByUsername(username);
+        const hitCache = await this.userIdCache.match(username);
+        const inActiveQueue = this.activeUserIdQueue[username];
+
+        if (inActiveQueue) {
+            return inActiveQueue;
+        }
+
+        if (hitCache) {
+            return hitCache.json();
+        } else {
+            const idPromise = devRant.getIdByUsername(username);
+            this.activeUserIdQueue[username] = idPromise;
+
+            const idResponse = await idPromise;
+
+            delete this.activeUserIdQueue[username];
+
+            const idCachableResponse = new Response(
+                JSON.stringify(idResponse)
+            );
+            this.userIdCache.put(username, idCachableResponse);
+
+            return idResponse;
+        }
     }
 
     async validateNotifications() {
@@ -75,8 +100,8 @@ export class DevRantService {
         }
     }
 
-    get profile() {
-        return this._profile;
+    get profile(): Partial<Profile> {
+        return this._profile || {};
     }
 
     get profileColor() {
@@ -101,6 +126,8 @@ export class DevRantService {
     async setupService() {
         const token = JSON.parse(localStorage.getItem('token'));
         await this.setupWorker();
+
+        this.userIdCache = await caches.open('username-id-map');
 
         if (token) {
             this.token = token;
@@ -127,6 +154,7 @@ export class DevRantService {
             token: this.token,
         });
 
+        this.getProfile();
         this.validateNotifications();
     }
 
@@ -146,12 +174,15 @@ export class DevRantService {
         if (theme) {
             theme.setAttribute('content', this.profileColor);
         }
+
+        const shades = makeShades(this.profileColor);
+        applyShadesTo(document.body, shades);
     }
 
     async getProfile(userId?: string, content?: string, skip?: number) {
         // we need to have a token AND the SAME userId as in the token OR no id and we use the id in the token.
-        if (this.token && (!userId || userId === String(this.token.user_id))) {
-            const request = this.lazyUpdateProfile(content, skip);
+        if (!this.token || !userId || userId === String(this.token.user_id)) {
+            const request = this.lazyUpdateLoggedInProfile(content, skip);
             if (!this.profile) {
                 await request;
             }
@@ -176,13 +207,19 @@ export class DevRantService {
         return response.profile;
     }
 
-    async lazyUpdateProfile(content?: string, skip?: number) {
-        this._profile = await this.fetchProfile(
-            String(this.token.user_id),
-            content,
-            skip
-        );
+    async lazyUpdateLoggedInProfile(content?: string, skip?: number) {
+        if (!this.token) {
+            this._profile = null;
+        } else {
+            this._profile = await this.fetchProfile(
+                String(this.token.user_id),
+                content,
+                skip
+            );
+        }
+
         this.profileUpdated();
+        return this._profile;
     }
 
     async logout() {
@@ -229,16 +266,6 @@ export class DevRantService {
 
     async getComment(commentId: number) {
         return devRant.comment(commentId, this.token);
-    }
-
-    /**
-     * postComment is a middle way service - no images for now
-     * @param rantId
-     * @param comment
-     * @param token
-     */
-    async postComment(rantId: number, comment: string, token: devRant.Token) {
-        return devRant.postComment(rantId, comment, null, token);
     }
 
     /**
